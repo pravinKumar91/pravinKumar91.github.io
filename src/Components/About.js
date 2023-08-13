36803048811d66244e47404ccb2a66b2ebba81b2
pravinKumar91 <prawanare@gmail.com>
2023-08-13 18:31:25 +0530

    json data path was incorrect

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
     if (!this.props.data) return null;
 
     const name = this.props.data.name;
-    const profilepic = "/resume/images/" + this.props.data.image;
+    const profilepic = "/images/" + this.props.data.image;
     const bio = this.props.data.bio;
     const street = this.props.data.address.street;
     const city = this.props.data.address.city;
